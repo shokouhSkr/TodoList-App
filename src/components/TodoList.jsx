ad178951e4f81e817bc07ca15b912ee1c437f512
shokouhSkr <shokouhskr@gmail.com>
2022-06-06 22:49:55 +0430

    TodoList component modified

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Todo from "./Todo";
 
-const TodoList = () => {
-  return <div></div>;
+const TodoList = ({ todos }) => {
+  const renderTodos = () => {
+    if (todos.length === 0) return <p>Add some todos</p>;
+
+    return todos.map((todo) => {
+      return <Todo key={todo.id} todo={todo} />;
+    });
+  };
+
+  return <div>{renderTodos()}</div>;
 };
 
 export default TodoList;
